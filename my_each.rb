d89c43a05dc6e60a90a2bba16ade4289bb98bd43
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-05-07 15:44:59 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,13 +4,13 @@
 def my_each(array) # put argument(s) here
   index = 0 
   while index < array.length 
-
      yield(array[index])
-
      index += 1    # i = i + 1 
   end
   array 
 end
+
+
 
 
 
