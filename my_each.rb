454fea8683a2fb01117477a7c9d46003555cba70
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-05-07 15:16:56 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,10 @@
 #return the original array 
 
 def my_each(array) # put argument(s) here
-  i = 0 
-  while i < array.length 
+  counter = 0 
+  while counter < array.length 
      
-     i += 1    # i = i + 1 
+     counter += 1    # i = i + 1 
   end
 end
 
